7c073777cbb3cdfc9d115dd2b91fcd5caa078191
ITSC <juju9634@gmail.com>
2023-07-02 14:51:26 +0900

    first commit

---
+++ .vscode/ex02-01-varuable.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main(void)
+{
+    int number1, number2;
+
+    number1 = 1;
+    number2 = 2;
+    int number3 = 3, number4= 4;
+    int number5 = 5;
+    printf("%d\n" , number1);
+    printf("%d\n" , number2);       
+    printf("%d\n" , number3);    
+    printf("%d\n" , number4);    
+    printf("%d\n" , number5);
+
+    int num =3;
+    int number6 =6;
+    int num_ber = 10;
+    int Number=15;
+    //<사용 불가 변수>;
+    //int hello boy = 3;
+    //int hi#$ = 5;
+    //int 8number = 8;
+    //int short = 3;
+
+    return 0;
+}
